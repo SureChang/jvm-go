b6635e430fe1beafe60e82bc5b207825131f81a5
bigmamonkey <zhangshuocool@163.com>
2017-07-18 22:15:40 +0800

    add structure and classpath entry impl.

---
+++ src/entry/entry.go
@@ -0,0 +1,15 @@
+package entry
+
+import "os"
+import "strings"
+
+const pathListSeparator = string(os.PathListSeparator)
+
+type Entry interface {
+	readClass(className string) ([]byte, Entry, error)
+	String() string
+}
+
+func newEntry(path string) Entry {
+	return newDirEntry(path)
+}
