bf4acde572e3404be2a6f37cb0a8f789d9f8ca4c
bigmamonkey <zhangshuocool@163.com>
2017-07-21 23:01:39 +0800

    add vm stack impl.

--- src/classReader/cp_class.go
+++ src/classReader/cp_class.go
@@ -4,9 +4,11 @@ type ConstantClassInfo struct {
 	cp ConstantPool
 	nameIndex uint16
 }
+
 func (self *ConstantClassInfo) readInfo(reader *ClassReader) {
 	self.nameIndex = reader.readUint16()
 }
+
 func (self *ConstantClassInfo) Name() string {
 	return self.cp.getUtf8(self.nameIndex)
 }
---
+++ src/rtda/frame.go
@@ -0,0 +1,15 @@
+package rtda
+
+type Frame struct {
+	lower *Frame
+	localVars LocalVars
+	operandStack *OperandStack
+}
+
+func NewFrame(maxLocals, maxStack uint) *Frame {
+	return &Frame{
+		localVars:
+		newLocalVars(maxLocals),
+		operandStack: newOperandStack(maxStack),
+	}
+}
